964f1bb0933a9a0a54449426f54890a76735ab51
mshaikh1415 <145638953+mshaikh1415@users.noreply.github.com>
2024-11-29 02:00:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_cloud():
-    return 'Hello Cloud! From Minaz Shaikh'
+    return 'Hello Cloud! From Minaz Shaikh - This is the updated file'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
